fef4e2fbc7ada22f468c39f8be0f5a5d8cba739c
osm3000 <omar.samir3000@gmail.com>
2017-11-12 23:29:41 +0100

    Modifications in order to adapt with the modifications made in the robot, ball, sensors and status classes

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import window
 from walls import *
+import robot
+from sensors import *
 # def update(dt):
 #     window.update()
 if __name__ == "__main__":
     print ("oh la laa")
+    my_robot = robot.Robot()
+    # ball = robot.Ball(ball_radius =40, color=(255, 255, 0))
+    my_robot.add_sensors(Sensors(sensor_coverage=180, num_rays=9, color=(0, 0, 255), sensor_range=60, center_angle=0))
+    my_robot.add_sensors(Sensors(sensor_coverage=110, num_rays=4, color=(255, 0, 0), sensor_range=60, center_angle=0))
     window = window.Window()
     # pyglet.clock.schedule_interval(window.update, 1/120.0)
-    window.add_walls(Wall(color=(0, 0, 255), wall_coordinates=[300, 0, 300, 600]))
-    window.add_walls(Wall(color=(255, 0, 0), wall_coordinates=[100, 0, 100, 600]))
+    window.add_env_objects(Wall(color=(0, 0, 255), wall_coordinates=[300, 0, 300, 600]))
+    window.add_env_objects(Wall(color=(255, 0, 0), wall_coordinates=[100, 0, 100, 600]))
+    window.add_env_objects(robot.Ball(ball_radius=40, color=(255, 0, 0)))
+    window.add_robot(my_robot)
     pyglet.app.run()
