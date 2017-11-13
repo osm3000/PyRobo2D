f5f85a8a2302c082637d6050852078576155a887
mohammom-admin <mohammom-admin@desktop-202.gipsa-lab.grenoble-inp.fr>
2017-11-13 08:09:04 +0100

    Make the information/messages as part of a dictionary + Add a call function (to ease the update of the messages)

--- robot_status.py
+++ robot_status.py
@@ -2,12 +2,21 @@ class RobotStatus:
     def __init__(self, log_robot_pos=True, log_robot_sensors=True):
         self.log_robot_pos = log_robot_pos
         self.log_robot_sensors = log_robot_sensors
-        self.robot_position = None
-        self.robot_rotation = None
-        self.robot_sensors_readings = None
+
+        #TODO: Instead of storing the info in separate variables, create one dictionary
+        # self.robot_position = None
+        # self.robot_rotation = None
+        # self.robot_sensors_readings = None
+        self.messages = dict(RobotPosition=None, RobotRotation=None, RobotSensorsReadings=None)
+
+    # def __call__(self, robot_position, robot_rotation, robot_sensors_readings):
+    def __call__(self, **kwargs):
+        self.messages['RobotPosition'] = kwargs['RobotPosition']
+        self.messages['RobotRotation'] = kwargs['RobotRotation']
+        self.messages['RobotSensorsReadings'] = kwargs['RobotSensorsReadings']
 
     def __str__(self):
-        final_string = "Robot position: " + str(self.robot_position) + "\n"
-        final_string += "Robot rotation: " + str(self.robot_rotation) + "\n"
-        final_string += "Sensors: " + str(self.robot_sensors_readings) + "\n"
+        final_string = ""
+        for key in self.messages:
+            final_string += key + ": " + str(self.messages[key]) + "\n"
         return final_string
