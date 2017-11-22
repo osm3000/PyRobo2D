223c63a04a9d90721c3a06dc617b1d0fef0da780
osm3000 <omar.samir3000@gmail.com>
2017-11-22 17:49:24 +0100

    Enabling the storage the robot history

--- robot_status.py
+++ robot_status.py
@@ -2,12 +2,13 @@ class RobotStatus:
     def __init__(self, log_robot_pos=True, log_robot_sensors=True):
         self.log_robot_pos = log_robot_pos
         self.log_robot_sensors = log_robot_sensors
-        self.robot_position = None
-        self.robot_rotation = None
-        self.robot_sensors_readings = None
+        self.robot_position = []
+        self.robot_rotation = []
+        self.robot_sensors_readings = []
 
     def __str__(self):
-        final_string = "Robot position: " + str(self.robot_position) + "\n"
-        final_string += "Robot rotation: " + str(self.robot_rotation) + "\n"
-        final_string += "Sensors: " + str(self.robot_sensors_readings) + "\n"
+        final_string = "History length: " + str(len(self.robot_rotation)) + "\n"
+        final_string += "Robot position: " + str(self.robot_position[-1]) + "\n"
+        final_string += "Robot rotation: " + str(self.robot_rotation[-1]) + "\n"
+        final_string += "Sensors: " + str(self.robot_sensors_readings[-1]) + "\n"
         return final_string
