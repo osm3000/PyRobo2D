9de7464fd3a53ab4e17a0552f58fffc15c353111
Omar Samir Mohammed <mohammom@desktop-364.gipsa-lab.grenoble-inp.fr>
2017-11-24 18:25:17 +0100

    Start building the final environment

--- main.py
+++ main.py
@@ -6,17 +6,26 @@ from agents import *
 # def update(dt):
 #     window.update()
 if __name__ == "__main__":
-    print ("oh la laa")
-    my_robot = robot.Robot(circle_radius=20, name="lovely_robot")
-    # ball = robot.Ball(ball_radius =40, color=(255, 255, 0))
+    my_robot = robot.Robot(circle_radius=20, name="lovely_robot", color=(0, 0, 0))
     my_robot.add_sensors(Sensors(sensor_coverage=180, num_rays=9, color=(0, 0, 255), sensor_range=60, center_angle=0, name="sensor_0"))
     my_robot.add_sensors(Sensors(sensor_coverage=110, num_rays=4, color=(255, 0, 0), sensor_range=60, center_angle=0, name="sensor_1"))
-    window = window.Window()
-    window.add_env_objects(Wall(color=(0, 0, 255), wall_coordinates=[300, 0, 300, 200], name="wall_1"))
-    window.add_env_objects(Wall(color=(0, 0, 255), wall_coordinates=[0, 200, 300, 200], name="wall_2"))
-    window.add_env_objects(Wall(color=(0, 0, 255), wall_coordinates=[300, 400, 300, 600], name="wall_3"))
-    window.add_env_objects(Wall(color=(255, 0, 0), wall_coordinates=[100, 0, 100, 600], name="wall_4"))
-    window.add_env_objects(robot.Ball(circle_radius=20, color=(255, 0, 0), name="little_ball"))
-    window.add_env_objects(robot.Ball(circle_radius=40, color=(0, 0, 255), name="my_basket", circle_position=[500, 500]))
+
+    width, heigth = 800, 800
+    window = window.Window(width=width, height=heigth)
+    window.add_env_objects(Wall(color=(0, 0, 255), wall_coordinates=[1, 1, 1, heigth-1], name="wall_1"))
+    window.add_env_objects(Wall(color=(0, 0, 255), wall_coordinates=[1, 1, width-1, 1], name="wall_2"))
+    window.add_env_objects(Wall(color=(0, 0, 255), wall_coordinates=[width-1, 1, width-1, heigth-1], name="wall_3"))
+    window.add_env_objects(Wall(color=(0, 0, 255), wall_coordinates=[1, heigth-1, width-1, heigth-1], name="wall_4"))
+
+    # window.add_env_objects(Wall(color=(0, 0, 255), wall_coordinates=[1, 1, 1, heigth-1], name="wall_8"))
+    # window.add_env_objects(Wall(color=(0, 0, 255), wall_coordinates=[1, 1, width-1, 1], name="wall_5"))
+    # window.add_env_objects(Wall(color=(0, 0, 255), wall_coordinates=[width-1, 1, width-1, heigth-1], name="wall_6"))
+    # window.add_env_objects(Wall(color=(0, 0, 255), wall_coordinates=[1, heigth-1, width-1, heigth-1], name="wall_7"))
+
+    # window.add_env_objects(robot.Ball(circle_radius=10, color=(255, 0, 0), name="ball_0", circle_position=[400, 400]))
+    # window.add_env_objects(robot.Ball(circle_radius=10, color=(255, 0, 0), name="ball_1", circle_position=[300, 300]))
+    # window.add_env_objects(robot.Ball(circle_radius=10, color=(255, 0, 0), name="ball_2", circle_position=[200, 200]))
+    # window.add_env_objects(robot.Ball(circle_radius=10, color=(255, 0, 0), name="ball_3", circle_position=[100, 100]))
+    # window.add_env_objects(robot.Ball(circle_radius=40, color=(0, 0, 255), name="basket", circle_position=[500, 500]))
     window.add_robot(my_robot)
     pyglet.app.run()
